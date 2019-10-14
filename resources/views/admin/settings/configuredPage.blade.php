14e9a7ae1b443d7b9298c48f611a68da9fa7e774
rimon <sh.rimon7@gmail.com>
2019-10-14 13:05:32 +0600

    work on settings configured page

--- resources/views/admin/settings/configuredPage.blade.php
+++ resources/views/admin/settings/configuredPage.blade.php
@@ -146,14 +146,10 @@
 
 @section('style')
     <link rel="stylesheet" href="{{ asset('assets/css/imageupload.css') }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/editor.css') }}">
 @stop
 
 @section('plugin')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src= "{{ asset('assets/js/editor.js') }}"></script>
 @stop
 
