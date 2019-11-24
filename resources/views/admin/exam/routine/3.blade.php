af92e9b59e9f33a51aaa9af3c169027b93c17b5d
rimon <sh.rimon7@gmail.com>
2019-11-24 17:07:35 +0600

    Examination Mgmt admit card n seat allocate.

---
+++ resources/views/admin/exam/routine/3.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.fixed')
+
+
+@section('contain')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered">
+                                <tr>
+                                    <td>English</td>
+                                    <td>27-11-19</td>
+                                    <td>10:00 AM</td>
+                                    <td>01:00 PM</td>
+                                    <td>100</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@stop
