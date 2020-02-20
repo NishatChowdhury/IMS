cf780df7c2a13c3a66547b8f28a149b156bba82d
arahmanbabu <gbabu.cox19@gmail.com>
2020-02-20 10:34:37 +0600

    Teacher front blade page

--- resources/views/front/pages/teacher.blade.php
+++ resources/views/front/pages/teacher.blade.php
@@ -16,7 +16,7 @@
                             <a href="#">Home</a>
                         </li>
                         <li class="breadcrumb-item">
-                            <a href="#"> Elements</a>
+                            <a href="#"> Team</a>
                         </li>
                         <li class="breadcrumb-item">
                             Teacher
@@ -31,15 +31,16 @@
     <section class="padding-y-100 wow fadeIn">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 marginTop-30">
+                @foreach($teachers as $teacher)
+                    <div class="col-lg-3 col-md-6 marginTop-30">
                     <div class="card height-100p border border-light text-center">
-                        <img class="card-img-top" src="assets/img/262x230/8.jpg" alt="">
+                        <img class="card-img-top" src="{{ asset('assets/img/staffs') }}/{{$teacher->image}}" alt="">
                         <div class="card-body">
                             <h4>
-                                John doe
+                                {{ ucfirst($teacher->name) }}
                             </h4>
                             <p class="mb-0">
-                                PHP Instructor
+                                {{ ucfirst($teacher->title) }}
                             </p>
                         </div>
                         <div class="card-footer border-top border-light">
@@ -60,97 +61,7 @@
                         </div>
                     </div>
                 </div> <!-- END col-lg-3 col-md-6-->
-
-                <div class="col-lg-3 col-md-6 marginTop-30">
-                    <div class="card height-100p border border-light text-center">
-                        <img class="card-img-top" src="assets/img/262x230/7.jpg" alt="">
-                        <div class="card-body">
-                            <h4>
-                                William
-                            </h4>
-                            <p class="mb-0">
-                                UI/UX Engineer
-                            </p>
-                        </div>
-                        <div class="card-footer border-top border-light">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-email"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-facebook"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-twitter"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-linkedin"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div> <!-- END col-lg-3 col-md-6-->
-
-                <div class="col-lg-3 col-md-6 marginTop-30">
-                    <div class="card height-100p border border-light text-center">
-                        <img class="card-img-top" src="assets/img/262x230/6.jpg" alt="">
-                        <div class="card-body">
-                            <h4>
-                                Alex lobby
-                            </h4>
-                            <p class="mb-0">
-                                Java developer
-                            </p>
-                        </div>
-                        <div class="card-footer border-top border-light">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-email"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-facebook"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-twitter"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-linkedin"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div> <!-- END col-lg-3 col-md-6-->
-
-                <div class="col-lg-3 col-md-6 marginTop-30">
-                    <div class="card height-100p border border-light text-center">
-                        <img class="card-img-top" src="assets/img/262x230/5.jpg" alt="">
-                        <div class="card-body">
-                            <h4>
-                                Jonathon Troat
-                            </h4>
-                            <p class="mb-0">
-                                Python Ninja
-                            </p>
-                        </div>
-                        <div class="card-footer border-top border-light">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-email"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-facebook"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-twitter"></i></a>
-                                </li>
-                                <li class="list-inline-item mr-0">
-                                    <a href="#" class="btn bg-light-v2 iconbox iconbox-sm rounded hover:primary"><i class="ti-linkedin"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div> <!-- END col-lg-3 col-md-6-->
-
+                @endforeach
             </div>
         </div> <!-- END container-->
     </section>
