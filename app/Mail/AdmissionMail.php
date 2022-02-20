a4c171947b084e67d7cd3a93054c7e80980db22b
Iftekhar Hossain <iftekhar0967@gmail.com>
2022-02-20 11:59:16 +0600

    add email vendor

--- app/Mail/AdmissionMail.php
+++ app/Mail/AdmissionMail.php
@@ -31,6 +31,6 @@ class AdmissionMail extends Mailable
     public function build()
     {
         return $this->subject('Mail from IMS')
-                    ->view('emails.admission-mail');
+                    ->markdown('emails.admission-mail');
     }
 }
