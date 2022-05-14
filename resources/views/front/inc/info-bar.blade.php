b84331bd65ddd1147d5f468adfe7467668d29336
smartrahat <smartrahat@gmail.com>
2022-05-14 17:26:22 +0600

    student layout

--- resources/views/front/inc/info-bar.blade.php
+++ resources/views/front/inc/info-bar.blade.php
@@ -16,15 +16,19 @@
         </ul>
         <ul class="list-inline mb-0">
             <li class="list-inline-item mr-0 p-3 border-right border-left border-white-0_1">
-                <span>EIIN: {{ siteConfig('eiin') }}</span>
+                <span>{{ __('EIIN') }}: {{ siteConfig('eiin') }}</span>
             </li>
             <li class="list-inline-item mr-0 p-3 border-right border-white-0_1">
-                <span>Institute Code: {{ siteConfig('code') }}</span>
+                <span>{{ __('Institute Code') }}: {{ siteConfig('code') }}</span>
             </li>
         </ul>
         <ul class="list-inline mb-0">
             <li class="list-inline-item mr-0 p-md-3 p-2 border-right border-left border-white-0_1">
-                <a href="{{ url('student/login') }}">Login</a>
+                @auth('student')
+                    Logout
+                @else
+                    <a href="{{ url('student/login') }}">{{ __('Login') }}</a>
+                @endauth
             </li>
         </ul>
     </div> <!-- END END row-->
