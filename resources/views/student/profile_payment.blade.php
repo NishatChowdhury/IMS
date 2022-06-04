4b821387f489df7534ed4b9bf7ef6e2a7e6295d5
smartrahat <smartrahat@gmail.com>
2022-06-04 16:09:28 +0600

    sslcommerz kickstart

--- resources/views/student/profile_payment.blade.php
+++ resources/views/student/profile_payment.blade.php
@@ -24,9 +24,26 @@
         <td>{{ __('Total Due') }}</td>
         <td>{{ number_format($due,2) }}</td>
         <td>
-            <button class="btn btn-info mr-2">Pay With Bkash</button>
-            <button class="btn btn-danger">Pay With Nagad</button>
+            <button class="your-button-class" id="sslczPayBtn"
+                    token="if you have any token validation"
+                    postdata="your javascript arrays or objects which requires in backend"
+                    order="If you already have the transaction generated for current order"
+                    endpoint="/pay-via-ajax"> Pay Now
+            </button>
         </td>
     </tr>
 </table>
 
+@push('js')
+    <script>
+        (function (window, document) {
+            var loader = function () {
+                var script = document.createElement("script"), tag = document.getElementsByTagName("script")[0];
+                script.src = "https://sandbox.sslcommerz.com/embed.min.js?" + Math.random().toString(36).substring(7);
+                tag.parentNode.insertBefore(script, tag);
+            };
+
+            window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+        })(window, document);
+    </script>
+@endpush
--- routes/student.php
+++ routes/student.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\SslCommerzPaymentController;
 use App\Http\Controllers\Student\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 
 Route::get('student/login','Student\ProfileController@showLoginForm')->name('student.login');
@@ -28,5 +30,20 @@ Route::prefix('student')->name('student.')->namespace('Student')->group(function
         Route::post('/password/reset','ResetPasswordController@reset')->name('password.update');
 //        student diary
     });
+
+
+// SSLCOMMERZ Start
+    Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+    Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+    Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+    Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+    Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+    Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+    Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+    Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 });
