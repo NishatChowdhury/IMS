83f1493903d739ac8f2a6ffa09a438689948c940
zisad <irin.cse.en@gmail.com>
2023-01-31 16:27:35 +0600

    lavender school design

--- resources/views/front/inc/title-bar.blade.php
+++ resources/views/front/inc/title-bar.blade.php
@@ -1,5 +1,6 @@
-{{--<section class="" data-primary-overlay="0" style="background-color:{{ themeConfig('header_background') }}">--}}
-<section class="firstblock" data-primary-overlay="0" >
+
+<!-- <section class="firstblock" data-primary-overlay="0" style="background-color:{{ themeConfig('header_background') }}"> -->
+<section  data-primary-overlay="0" style="background-color:{{ themeConfig('header_background') }}"> >
 <div class="container">
         <div class="row">
             <div class="col-md-2 text-center d-md-flex align-items-center">
@@ -10,7 +11,8 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-10 text-blue text-center">
+            <div class="col-md-10 text-blue text-center"
+            >
                 <h2 class="mb-4">
                     <span style="color:{{ siteConfig('name_color') }};font-size:{{ siteConfig('name_size') }}px;font-family:{{ siteConfig('name_font') }};">{{ siteConfig('name') }}</span><br>
                     <span style="color:{{ siteConfig('bn_color') }};font-size:{{ siteConfig('bn_size') }}px;font-family:{{ siteConfig('bn_font') }};">{{ siteConfig('bn') }}</span>
--- resources/views/front/result.blade.php
+++ resources/views/front/result.blade.php
@@ -1,8 +1,10 @@
+<!-- <section class=" padding-y-100 bg-black-0_9"> -->
 <section class=" padding-y-100 bg-black-0_9">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-5 mr-auto my-4 text-white wow slideInUp">
-                <h2>{{ __('Subscribe to') }} <span class="text-primary">{{ __('Newsletter')}}</span></h2>
+            <!-- <div class="col-lg-5 mr-auto my-4 text-white wow slideInUp"> -->
+            <div class="col-lg-5 mr-auto my-4  text-white wow slideInUp">
+                <h2 class="resultext">{{ __('Subscribe to') }} <span class=" textt">{{ __('Newsletter')}}</span></h2>
                 <p class="lead">
                     {{ __('Get notified about new courses, events, community & more')}}
                 </p>
@@ -16,7 +18,8 @@
                     </div>
                     <input type="text" name="email" placeholder="Enter your email" class="form-control py-3 pl-0 border-white" required>
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="submit">{{ __('Subscribe')}}</button>
+                        <!-- <button class="btn btn-primary lavenderround" type="submit">{{ __('Subscribe')}}</button> -->
+                        <button class="btn  btn-primary " type="submit">{{ __('Subscribe')}}</button>
                     </div>
                 </div>
                 </form>
