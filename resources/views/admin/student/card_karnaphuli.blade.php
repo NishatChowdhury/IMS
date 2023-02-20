7049ffd2aa5c4ee6ed98851f2232577cfb94a043
Nishat Chowdhury <nishatchowdhury73@gmail.com>
2023-02-20 17:13:23 +0600

    Push about new Updates on KAJ ID Card.

--- resources/views/admin/student/card_karnaphuli.blade.php
+++ resources/views/admin/student/card_karnaphuli.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPIMS</title>
+    <title>{{ __('WPIMS') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -88,51 +88,40 @@
             background: #f7f7f7;
         }
 
-
-
         .grid-container {
             position: relative;
             top: 1px;
-            /* bottom: -87px; */
             border-radius: 10px;
             display: grid;
-            /* grid-template-columns: auto auto auto auto; */
-
             background-color: #fff;
-
         }
 
         .grid-container>div {
 
             text-align: center;
             height: 30px;
-
-
         }
 
         .item1 {
 
             grid-column: 1/ span 2;
             border-radius: 0px 0px 0px 5px;
-            background-color: rgb(16, 100, 21);
+            background-color: #00AD51;
             font-size: 10px;
             color: #fff;
-            
         }
 
         .item2 {
             grid-column: 3/ span 3;
             border-radius: 0px 0px 5px 0px;
-            background-color: rgb(216, 29, 29);
+            background-color: #FE3501;
             font-size: 13px;
-
         }
 
         .item2 span {
             top: 3px;
             position: relative;
             color: #fff;
-
         }
 
         .logoo3 {
@@ -141,7 +130,6 @@
             width: 140px;
             bottom: 20px;
             right: -35px;
-
         }
 
         .logoo3 img {
@@ -155,12 +143,11 @@
 
 </head>
 
-
 @foreach ($students->chunk(5) as $key => $chunk)
     <div class="row" style="{{ ($key + 1) % 4 == 0 ? 'page-break-after: always' : '' }}">
         @foreach ($chunk as $student)
-            <div class="col-3 col-2-5" {{-- style="max-width:260px; max-height:375px" --}}>
-                <div class="card text-center" style="width: 2.17in;height:4.18in">
+            <div class="col-3 col-2-5">
+                <div class="card text-center" style="width: 2.17in;height:4.42in">
                     <div class="card-header bg-white border-0"
                         style="padding:10px 0 0 10px;background-color:{{ $card['bgcolor'] }};color:{{ $card['bgfont'] }}">
                         <div class="row">
@@ -175,27 +162,38 @@
                                 <div class="right text-center">
                                     <div class="scl-cd-dec text-wrap text-bold">
                                         <h2
-                                            style="padding-top: 3px; position: relative;top: 4px;margin: 2px;text-align: center; font-size:14px ;color:rgb(104, 8, 12)">
-                                            <strong> {{ siteConfig('name') }}</strong></h2>
+                                            style="padding-top: 3px; position: relative;top: 4px;margin: 2px;text-align: center; font-size:14px; font-weight:500 ;color:#9A001B">
+                                            <strong> {{ siteConfig('name') }}</strong>
+                                        </h2>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
                     <div class="card-body">
-                        <img src="{{ asset('storage/uploads/students/') }}/{{ $student->student->image }}"
-                            width="70" alt=""
-                            style="border: 2px solid #000;min-height: 70px;max-height: 90px"
-                            class="text-center rounded mx-auto d-block border border-dark">
+                        <figure class="figure ">
+                            <figcaption class="figure-caption text-center border border-dark p-0"
+                                style="height: 20px;position: relative;position: relative;background-color: green;color: white;width: 70px;">
+                                {{ __(' ID Card') }}
+                            </figcaption>
+                            <img src="{{ asset('storage/uploads/students/') }}/{{ $student->student->image }}"
+                                width="70" alt=""
+                                style="border: 2px solid #000;min-height: 70px;max-height: 90px"
+                                class="text-center  mx-auto d-block ">
+
+                        </figure>
+
+
                         @isset($card['nickname'])
                             <h2
-                                style="padding-top: 3px;    position: relative;top: 4px;margin: 2px;text-align: center; font-size:18px ;color:rgb(4, 34, 133)">
-                                <strong> {{ $student->student->name }} </strong></h2>
+                                style="padding-top: 3px; position: relative;top: -10px;margin: 2px;text-align: center; font-size:18px ;color:#042E57">
+                                <strong> {{ $student->student->name }} </strong>
+                            </h2>
                         @endisset
                         <table class="table"
-                            style="text-align:left;font-size:{{ $card['body_size'] != null ? $card['body_size'] : 0 }}px">
+                            style="text-align:left;font-size:{{ $card['body_size'] != null ? $card['body_size'] : 0 }}px;     position: relative;
+                            left: 8px;
+                            line-height : 11px">
                             <tbody>
                                 @isset($card['fullname'])
                                     <tr>
@@ -266,11 +264,16 @@
                                         <td>{{ $student->student->dob }}</td>
                                     </tr>
                                 @endisset
+                                <tr>
+                                    <td><strong>{{ __('Session') }}</strong></td>
+                                    <td>&nbsp;:&nbsp;</td>
+                                    <td>{{ __('2022-2023') }}</td>
+                                </tr>
                                 @isset($card['blood'])
                                     <tr>
                                         <td><strong>{{ __('Blood Group') }}</strong></td>
                                         <td>&nbsp;:&nbsp;</td>
-                                        <td>{{ $student->student->blood->name ?? '' }}</td>
+                                        <td>{{ $student->student->bloodGroup->name ?? '' }}</td>
                                     </tr>
                                 @endisset
                                 @isset($card['contact'])
@@ -280,21 +283,19 @@
                                         <td>{{ $student->student->mobile }}</td>
                                     </tr>
                                 @endisset
-                                <tr>
-                                    <td><strong>{{ __('Validity') }}</strong> </td>
-                                    <td>&nbsp;:&nbsp;</td>
-                                    <td><strong>{{ __('31 June 2024') }}</strong></td>
-                                </tr>
                             </tbody>
                         </table>
-
                     </div>
-                    <div class="card-footer" style="float:right;background-color:transparent;border:none">
 
-                        <div class="col-4 ml-4" style="float: right; padding-right:3px">
-                            <img src="{{ asset('assets/img/signature/signature.png') }}" width="40" alt="">
+                    <div class="card-footer row" style="float:right;background-color:transparent;border:none">
+                        <div class="col text-center ml-3" style="float:right; padding-right:2px">
+                            <h6 class="text-center" style=" font-size:9px;margin-top: 12px;">{{ __('Validity:') }}<span
+                                    style="color:red"> {{ __('30 June 2024') }}</span></h6>
                         </div>
-
+                        <div class="col" style="float:right; padding-right:3px ;padding-top:2px">
+                            <img src="{{ asset('assets/img/signature/kaj_signature.png') }}" width="80"
+                                alt="">
+                        </div>
                     </div>
                     <div class="card-footer text-muted"
                         style="background-color:{{ $card['bgcolor'] }};font-size:{{ $card['body_size'] != null ? $card['body_size'] : 0 }}px">
