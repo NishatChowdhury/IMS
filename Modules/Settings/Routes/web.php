cde42a0d0ae134929e543e9fc35c28091c5423ab
smartrahat <smartrahat@gmail.com>
2023-02-23 17:32:45 +0600

    Laravel Modules

---
+++ Modules/Settings/Routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Settings\Http\Controllers\LinkController;
+
+//Route::prefix('settings')->group(function() {
+//    Route::get('/', 'SettingsController@index')->name('settings');
+//
+//        Route::get('links',[LinkController::class,'index'])->name('settings.link.index');
+//});
+
+//// Important Links
+//Route::get('settings/links','Backend\LinkController@index')->name('setting.index');
+//Route::post('settings/link/store','Backend\LinkController@store')->name('setting.store');
+//Route::delete('settings/link/delete/{id}','Backend\LinkController@destroy')->name('setting.destroy');
+//// End Important Links
+//
+//
+////Settings Route by Rimon
+//Route::get('settings/basicInfo','Backend\SettingsController@basicInfo')->name('settings.basicInfo');
